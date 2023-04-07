b15f7d47cc170a4d836e2975588fda2cf61ea9d9
Melpositivity <emeldahamatengo@gmail.com>
2023-04-07 17:56:53 +0300

    chamges

--- translator/models.py
+++ translator/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from audio_recorder.models import AudioRecord
+
+
+class Recording(models.Model):
+    audio_file = models.ForeignKey(AudioRecord, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
