a32c80b014a2cb86c4a3dae44de36425715f29f2
FREDERICO23 <adhingafredrick@gmail.com>
2023-04-12 18:05:19 +0300

    Update translator urls

--- translator/urls.py
+++ translator/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('/translator', views.index, name='index'),
     path("", views.homepage, name='home'),
 
 ]
